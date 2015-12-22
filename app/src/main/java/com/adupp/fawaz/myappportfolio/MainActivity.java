4f526cdf441061d984b1697e160f9e19da0810f5
fawaz <fawazeez@gmail.com>
2015-12-21 22:44:12 -0800

    button click code

--- app/src/main/java/com/adupp/fawaz/myappportfolio/MainActivity.java
+++ app/src/main/java/com/adupp/fawaz/myappportfolio/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +20,77 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button button_app1 = (Button) findViewById(R.id.button_app1);
+        button_app1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "THIS BUTTON WILL LAUNCH MY " + getString(R.string.app1), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v, R.string.app1, Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        Button button_app2a = (Button) findViewById(R.id.button_app2a);
+        button_app2a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "THIS BUTTON WILL LAUNCH MY "+ getString(R.string.app2a), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,R.string.app2a, Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        Button button_app2b = (Button) findViewById(R.id.button_app2b);
+        button_app2b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "THIS BUTTON WILL LAUNCH MY "+ getString(R.string.app2b), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,R.string.app2b, Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+
+            }
+        });
+
+        Button button_app3 = (Button) findViewById(R.id.button_app3);
+        button_app3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "THIS BUTTON WILL LAUNCH MY " + getString(R.string.app3), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,R.string.app3, Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+
+            }
+        });
+        Button button_app4 = (Button) findViewById(R.id.button_app4);
+        button_app4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "THIS BUTTON WILL LAUNCH MY " + getString(R.string.app4), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,R.string.app4, Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+
+            }
+        });
+        Button button_app5 = (Button) findViewById(R.id.button_app5);
+        button_app5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "THIS BUTTON WILL LAUNCH MY "+  getString(R.string.app5), Toast.LENGTH_SHORT).show();
+                Snackbar.make(v,R.string.app5, Snackbar.LENGTH_SHORT)
+                        .setAction("Action", null).show();
+
+            }
+        });
+       /* fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+        */
     }
 
     @Override
